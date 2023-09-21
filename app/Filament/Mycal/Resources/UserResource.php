e48ded05a029e10e5914934893bab5a73dc9ab85
litedz <maamarjoker@gmail.com>
2023-09-21 06:56:17 +0100

    wip

--- app/Filament/Mycal/Resources/UserResource.php
+++ app/Filament/Mycal/Resources/UserResource.php
@@ -5,6 +5,7 @@ namespace App\Filament\Mycal\Resources;
 use App\Events\ContactUserEvent;
 use App\Filament\Mycal\Resources\UserResource\Pages;
 use App\Jobs\ProcessContactUsers;
+use App\Models\profile;
 use App\Models\User;
 use Filament\Forms\Components\MarkdownEditor;
 use Filament\Forms\Components\TextInput;
@@ -46,6 +47,7 @@ class UserResource extends Resource
             ])
             ->actions([
                 Action::make('Contact')
+
                     ->fillForm(fn (User $record): array => [
                         'name' => $record->name,
                         'email' => $record->email,
@@ -62,11 +64,32 @@ class UserResource extends Resource
                         ContactUserEvent::dispatch($data['email'], $data['name'], $data['subject']);
                     })
                     ->link(),
-                Tables\Actions\EditAction::make()->action(fn (array $data) => dd($data)),
+                Tables\Actions\EditAction::make(),
                 ViewAction::make()
+                    ->mutateRecordDataUsing(function (array $data, Builder $builder): array {
+                        $profile =profile::where('id', $data['profile_id'])->first();
+                        $data['result'] = $profile->result;
+                        $data['age'] = $profile->age;
+                        $data['sex'] = $profile->sex;
+                        $data['weight'] = $profile->weight;
+                        $data['unitWeight'] = $profile->unitWeight;
+                        $data['height'] = $profile->height;
+                        $data['unitHeight'] = $profile->unitHeight;
+                        $data['activity'] = $profile->activity;
+                        $data['result'] = $profile->result;
+
+                        return $data;
+                    })
                     ->form([
                         TextInput::make('name'),
-                        TextInput::make('profile.result'),
+                        TextInput::make('age'),
+                        TextInput::make('sex'),
+                        TextInput::make('weight'),
+                        TextInput::make('unitWeight'),
+                        TextInput::make('height'),
+                        TextInput::make('unitHeight'),
+                        TextInput::make('activity'),
+                        TextInput::make('result'),
                     ]),
             ])
             ->bulkActions([
@@ -74,20 +97,20 @@ class UserResource extends Resource
                     Tables\Actions\DeleteBulkAction::make(),
                 ]),
                 Tables\Actions\BulkAction::make('Contact all')
-                ->form([
-                    TextInput::make('subject'),
-                    MarkdownEditor::make('message'),
-                ])
-                ->button()
-                ->color('primary')
-                ->icon('heroicon-o-envelope')
-                ->action(
-                    function (Collection $records, array $data) {
-                        $records->each(function ($user) use ($data) {
-                            ProcessContactUsers::dispatchSync($user->email, $user->name, $data['subject'], $data['message']);
-                        });
-                    }
-                ),
+                    ->form([
+                        TextInput::make('subject'),
+                        MarkdownEditor::make('message'),
+                    ])
+                    ->button()
+                    ->color('primary')
+                    ->icon('heroicon-o-envelope')
+                    ->action(
+                        function (Collection $records, array $data) {
+                            $records->each(function ($user) use ($data) {
+                                ProcessContactUsers::dispatchSync($user->email, $user->name, $data['subject'], $data['message']);
+                            });
+                        }
+                    ),
             ]);
     }
     public static function getPages(): array
